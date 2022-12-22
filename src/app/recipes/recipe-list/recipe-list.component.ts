c855a3251afa6ac4fa55d03dd97bede21d038801
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-22 10:49:17 -0500

    outputs list of recipes and displays details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    //this will make new recipes based on the recipe class that we created in the recipes.model
+    new Recipe('test cookie', 'yummy cookies','https://upload.wikimedia.org/wikipedia/commons/f/f1/2ChocolateChipCookies.jpg')
+  ];
 
   constructor() { }
 
